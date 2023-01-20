cc5a173b7d2dd9770a3084f8e970f2372f587774
jso4342 <jso4342@naver.com>
2023-01-20 09:41:14 +0900

    feat : Add JPA Auditing

--- src/main/java/com/jpa/board/domain/user/entity/BaseEntity.java
+++ src/main/java/com/jpa/board/domain/user/entity/BaseEntity.java
@@ -1,15 +1,22 @@
 package com.jpa.board.domain.user.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseEntity {
     @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @Column(name = "created_by")
+    @CreatedBy
     private String createdBy;
 }
