b66786512e285fe439ec3a66944916eccb0b5150
jso4342 <jso4342@naver.com>
2023-01-20 14:41:22 +0900

    fix : 오타 수정

--- src/main/java/com/jpa/board/domain/post/PostService.java
+++ src/main/java/com/jpa/board/domain/post/PostService.java
@@ -1,9 +1,11 @@
-package com.jpa.board.domain.post.service;
+package com.jpa.board.domain.post;
+
+import com.jpa.board.domain.entities.User;
+import com.jpa.board.domain.user.UserRepository;
+import com.jpa.board.domain.user.UserService;
+import com.jpa.board.dtos.PostDto.*;
 
 import com.jpa.board.domain.entities.Post;
-import com.jpa.board.dtos.PostDto;
-import com.jpa.board.dtos.PostsDto;
-import com.jpa.board.domain.post.repository.PostRepository;
 import com.jpa.board.exceptions.ResourceNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,28 +15,43 @@ import org.springframework.transaction.annotation.Transactional;
 import java.text.MessageFormat;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
-    public PostService(PostRepository postRepository){
+    public PostService(PostRepository postRepository, UserRepository userRepository){
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
-    public PostsDto getPosts(Pageable pageable) {
+    // 게시글 조회 (페이징)
+    @Transactional(readOnly = true)
+    public GetPostsResponse getPosts(Pageable pageable) {
         Page<Post> postPage = postRepository.findAll(pageable);
 
-        return new PostsDto(
+        return new GetPostsResponse(
                 postPage.stream()
-                        .map(PostDto::from)
+                        .map(GetPostDetailsResponse::from)
                         .toList()
         );
     }
 
-    public PostDto getPostDetail(long postId) {
+    // 게시글 조회 (단건)
+    @Transactional(readOnly = true)
+    public GetPostDetailsResponse getPostDetail(long postId) {
         Post savedPost = postRepository.findById(postId)
                 .orElseThrow(() -> new ResourceNotFoundException(MessageFormat.format("Cannot find the relevant Post. Please check post id. [Post ID]: {0}", String.valueOf(postId))));
 
-        return PostDto.from(savedPost);
+        return GetPostDetailsResponse.from(savedPost);
+    }
+
+    // 게시글 작성
+    public long createPost(CreatePostRequest createPostRequest) {
+        User user = userRepository.findById(createPostRequest.id())
+                .orElseThrow(() -> new ResourceNotFoundException(MessageFormat.format("Can not find User. Please check user id. [User ID]: {0}", createPostRequest.id())));
+
+        Post post = postRepository.save(createPostRequest.toEntity(user));
+        return post.getId();
     }
 }
--- src/main/java/com/jpa/board/dtos/PostDto.java
+++ src/main/java/com/jpa/board/dtos/PostDto.java
@@ -1,13 +1,53 @@
 package com.jpa.board.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jpa.board.domain.entities.Post;
+import com.jpa.board.domain.entities.User;
+import jakarta.validation.constraints.NotBlank;
 
-public record PostDto (
-        long id,
-        String title,
-        String content
-){
-    public static PostDto from (Post post) {
-        return new PostDto(post.getId(), post.getTitle(), post.getContent());
+import java.util.List;
+
+//public record PostDto (
+//        long id,
+//        @NotBlank String title,
+//        @NotBlank String content
+//){
+//    public static PostDto from (Post post) {
+//        return new PostDto(post.getId(), post.getTitle(), post.getContent());
+//    }
+//
+//    public CreatePostRequest(@JsonProperty("title") String title, @JsonProperty("content") String content, @JsonProperty("userId") long id) {
+//        this.title = title;
+//        this.content = content;
+//        this.id = id;
+//    }
+//
+//    public Post toEntity(User user) {
+//        return new Post(this.title, this.content, user);
+//    }
+//
+//}
+
+public class PostDto {
+
+    public record GetPostsResponse(List<GetPostDetailsResponse>postDetails) { }
+
+    public record GetPostDetailsResponse(long id, String title, String content) {
+        public static GetPostDetailsResponse from(Post post) {
+            return new GetPostDetailsResponse(post.getId(), post.getTitle(), post.getContent());
+        }
+    }
+
+    public record CreatePostRequest(@NotBlank String title, @NotBlank String content, long id) {
+
+        public CreatePostRequest(@JsonProperty("title") String title, @JsonProperty("content") String content, @JsonProperty("userId") long id) {
+            this.title = title;
+            this.content = content;
+            this.id = id;
+        }
+
+        public Post toEntity(User user) {
+            return new Post(this.title, this.content, user);
+        }
     }
 }
