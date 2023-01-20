6ad5c525bfb0eba9b47aace820a9d1730043ac23
jso4342 <jso4342@naver.com>
2023-01-20 09:38:42 +0900

    refactor : Delete unused import

--- src/main/java/com/jpa/board/domain/user/entity/User.java
+++ src/main/java/com/jpa/board/domain/user/entity/User.java
@@ -1,17 +1,12 @@
 package com.jpa.board.domain.user.entity;
 
-import com.jpa.board.domain.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -33,6 +28,4 @@ public class User {
 
     @Column(nullable = true, length = 3)
     private int age;
-
-
 }
