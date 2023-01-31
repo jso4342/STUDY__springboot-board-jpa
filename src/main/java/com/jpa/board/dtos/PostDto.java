0a8690a58c356b0d3d0c4fdc9080623e378a5e22
jso4342 <jso4342@naver.com>
2023-02-01 05:48:35 +0900

    style : Dto 코딩 컨벤션 번경

--- src/main/java/com/jpa/board/dtos/PostDto.java
+++ src/main/java/com/jpa/board/dtos/PostDto.java
@@ -6,17 +6,33 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class PostDto {
-    public record PostRequest(@NotNull long userId, @NotBlank String title, @NotBlank String content) {
+    public record PostRequest(
+            @NotNull
+            long userId,
+            @NotBlank
+            String title,
+            @NotBlank
+            String content
+    ) {
         public Post toEntity(User user) {
             return new Post(title, content, user);
         }
     }
 
-    public record PostResponse(Long postId, String title, String content) {
+    public record PostResponse(
+            Long postId,
+            String title,
+            String content
+    ) {
         public static PostResponse from(Post post) {
             return new PostResponse(post.getId(), post.getTitle(), post.getContent());
         }
     }
 
-    public record ModifyRequest(@NotBlank String title, @NotBlank String content) { }
+    public record ModifyRequest(
+            @NotBlank
+            String title,
+            @NotBlank
+            String content
+    ) { }
 }
--- src/main/java/com/jpa/board/dtos/UserDto.java
+++ src/main/java/com/jpa/board/dtos/UserDto.java
@@ -4,13 +4,22 @@ import com.jpa.board.domain.entities.User;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserDto {
-    public record UserRequest(@NotBlank String name, String hobby, int age) {
+    public record UserRequest(
+            @NotBlank
+            String name,
+            String hobby,
+            int age
+    ) {
         public User toEntity() {
             return new User(name, hobby, age);
         }
     }
 
-    public record UserResponse(Long userId, String name, String hobby, int age) {
+    public record UserResponse(
+            Long userId,
+            String name,
+            String hobby,
+            int age) {
         public static UserResponse from(User user) {
             return new UserResponse(user.getId(), user.getName(), user.getHobby(), user.getAge());
         }
