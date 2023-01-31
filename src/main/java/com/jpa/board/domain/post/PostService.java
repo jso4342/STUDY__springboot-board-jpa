44a9d0e8d23f5aff3d4173bb16fabee6e06b4798
jso4342 <jso4342@naver.com>
2023-02-01 04:46:24 +0900

    refactor : PostService 중복 코드 메서드로 추출

--- src/main/java/com/jpa/board/domain/post/PostService.java
+++ src/main/java/com/jpa/board/domain/post/PostService.java
@@ -2,15 +2,16 @@ package com.jpa.board.domain.post;
 
 import com.jpa.board.domain.entities.User;
 import com.jpa.board.domain.user.UserRepository;
+import com.jpa.board.domain.user.UserService;
 import com.jpa.board.dtos.PostDto.*;
 import com.jpa.board.domain.entities.Post;
 import com.jpa.board.exceptions.ResourceNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.MessageFormat;
 import java.util.List;
 
 @Service
@@ -19,7 +20,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-    public PostService(PostRepository postRepository, UserRepository userRepository){
+    public PostService(PostRepository postRepository, UserRepository userRepository, UserService userService){
         this.postRepository = postRepository;
         this.userRepository = userRepository;
     }
@@ -35,8 +36,7 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public PostResponse getPostById(Long postId) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new ResourceNotFoundException("해당 게시글을 찾을 수 없습니다."));
+        Post post = findPostById(postId);
 
         return PostResponse.from(post);
     }
@@ -49,11 +49,16 @@ public class PostService {
         return PostResponse.from(post);
     }
 
-    public PostResponse modifyPost(Long id, ModifyRequest request){
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(MessageFormat.format("해당 게시글을 찾을 수 없습니다.", id)));
+    public PostResponse modifyPost(Long postId, ModifyRequest request){
+        Post post = findPostById(postId);
         post.updatePost(request.title(), request.content());
 
         return PostResponse.from(post);
+    }
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+     public Post findPostById(Long postId){
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 게시글을 찾을 수 없습니다."));
     }
 }
